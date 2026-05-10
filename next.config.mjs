3110f5f78254ac1e94efb62089ac1f465cd44d8d
Lee Quessenberry <lquessenberry@users.noreply.github.com>
2026-05-09 22:07:41 -0500

    Setup GitHub Pages with static export for Next.js + Puck boilerplate

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
+  output: 'export',
+  basePath: '/puck-bem-atomic-nextjs',
+  trailingSlash: true,
 };
+
 export default nextConfig;
